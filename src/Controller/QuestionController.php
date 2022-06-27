7c99669734ed5c541c540d970e57bd1e1737ce20
Rachid Jeffali <rachid.jeffali@wassati.com>
2022-06-27 11:15:14 +0200

    Mise en place du formulaire de la question

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Form\QuestionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $formQuestion = $this->createForm(QuestionType::class);
+        $formQuestion->handleRequest($request);
+
+        if($formQuestion->isSubmitted() && $formQuestion->isValid()) {
+            // dump($formQuestion->getData());
+        }
+
         return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
+            'form' => $formQuestion->createView(),
         ]);
     }
 }
---
+++ src/Form/QuestionType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
